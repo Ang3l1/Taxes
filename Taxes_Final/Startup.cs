d25f0750be70a19eb225828f3a8e284ee19263b1
Angel Omar Robles Rodrigues <darkgravem@gmail.com>
2018-03-31 14:18:51 -0300

    ...

---
+++ Taxes_Final/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Taxes_Final.Startup))]
+namespace Taxes_Final
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
